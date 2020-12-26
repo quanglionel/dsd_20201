e47a4ecc0a21dd15aa25cb584f11dcafc42aa13d
HUST\Quang.SHV167972 <quang.shv167972@sis.hust.edu.vn>
2020-12-26 11:46:38 +0700

    no message

--- src/pages/DSD_05_De_Dieu/Task5/index.jsx
+++ src/pages/DSD_05_De_Dieu/Task5/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Tabs} from 'antd';
+import UserManager from './UserManager';
 function callback(key){
     console.log(key);
 }
+
 function Task5(){
     const { TabPane } = Tabs;
     return(
         <Tabs defaultActiveKey="1" onChange={callback} style={{height:200}}>
-            <TabPane tab="Quản lý người dùng" key="1">HEllo</TabPane>
+            <TabPane tab="Quản lý người dùng" key="1"><UserManager /></TabPane>
             <TabPane tab="Quản lý phòng ban" key="2">Hi</TabPane>
             <TabPane tab="Quản lý chức vụ" key="3">HEllo</TabPane>
             <TabPane tab="Quản lý quyền" key="4">HEllo</TabPane>
